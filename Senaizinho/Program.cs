9cc3372310465eb179acf4132723f5095ce2daef
Matheus Amaral De Camargo Cardoso Lima <41813664803@info.local>
2019-04-16 11:22:34 -0300

    senaizinho2

--- Senaizinho/Program.cs
+++ Senaizinho/Program.cs
@@ -8,83 +8,121 @@ namespace Senaizinho
         {
             bool continuar = true;
             int i = 0;
-            int contSala = 0;
-            Aluno[] aluno = new Aluno[10];
+            int cont = 0;
+            Aluno[] alunos = new Aluno[4];
+            Sala[] salas = new Sala[1];
+            int cadastrados = 0;
 
-
-            Console.WriteLine("-----Bem vindo à Secretaria Virtual-----");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("----- Bem vindo à Secretaria Virtual -----");
+            Console.ResetColor();
 
             while (continuar)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 System.Console.WriteLine("1 - Cadastrar Aluno");
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 System.Console.WriteLine("2 - Cadastrar Sala");
+                Console.ForegroundColor = ConsoleColor.Green;
                 System.Console.WriteLine("3 - Alocar Aluno");
+                Console.ForegroundColor = ConsoleColor.Blue;
                 System.Console.WriteLine("4 - Remover Aluno");
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
                 System.Console.WriteLine("5 - Verificar Salas");
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 System.Console.WriteLine("6 - Verificar Alunos");
+                Console.ForegroundColor = ConsoleColor.Red;
                 System.Console.WriteLine("0 - Sair");
+                Console.ResetColor();
 
                 string resposta = Console.ReadLine();
-                
+
                 switch (resposta)
                 {
                     case "1":
-                        if (i < 5)
+                        if (i < 4)
                         {
-                        Aluno aluno1 = new Aluno();
-                        System.Console.WriteLine("Digite o nome do aluno a ser cadastrado");
-                        aluno1.nome = Console.ReadLine();
-                        System.Console.WriteLine("Insira a data de nascimento do aluno");
-                        aluno1.dataNascimento = System.DateTime.Parse(Console.ReadLine());
-                        System.Console.WriteLine("Digite o curso do aluno");
-                        aluno1.Curso = Console.ReadLine();
-                        System.Console.WriteLine("Digite o número da sala do aluno");
-                        aluno1.numeroSala = int.Parse(Console.ReadLine());
-                        System.Console.WriteLine("-----Aluno cadastrado-----");
-                        
-                        i++;
-                        }else{
-                            System.Console.WriteLine("-----Número máximo de alunos cadastrados atingido-----");
+                            Aluno aluno = new Aluno();
+                            System.Console.WriteLine("Digite o nome do aluno a ser cadastrado");
+                            aluno.nome = Console.ReadLine();
+                            System.Console.WriteLine("Insira a data de nascimento do aluno");
+                            aluno.dataNascimento = System.DateTime.Parse(Console.ReadLine());
+                            System.Console.WriteLine("Digite o curso do aluno");
+                            aluno.Curso = Console.ReadLine();
+                            System.Console.WriteLine("Digite o número da sala do aluno");
+                            aluno.numeroSala = int.Parse(Console.ReadLine());
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            System.Console.WriteLine("----- Aluno cadastrado -----");
+                            Console.ResetColor();
+                            alunos[cadastrados] = aluno;
+                            cadastrados += 1;
+                            i++;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            System.Console.WriteLine("----- Número máximo de alunos cadastrados atingido -----");
+                            Console.ResetColor();
                         }
                         break;
                     case "2":
-                        if (contSala < 2)
+                        if (cont < 1)
                         {
-                        Sala sala1 = new Sala();
-                        System.Console.WriteLine("Digite o número da sala a ser cadastrada");
-                        sala1.numeroSala = int.Parse(Console.ReadLine());
-                        System.Console.WriteLine("-----Sala cadastrada-----");
-                        contSala++;
-                        }else{
-                            System.Console.WriteLine("-----Número máximo de salas cadastradas-----");
+                            Sala sala1 = new Sala();
+                            System.Console.WriteLine("Digite o número da sala a ser cadastrada");
+                            sala1.numeroSala = int.Parse(Console.ReadLine());
+                            System.Console.WriteLine("Digite a capacidade total da sala");
+                            sala1.capacidadeTotal = int.Parse(Console.ReadLine());
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            System.Console.WriteLine("----- Sala cadastrada -----");
+                            Console.ResetColor();
+                            salas[cont] = sala1;
+                            sala1.Alocar(alunos[i].nome);
+                            cont += 1;
+                            cont++;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            System.Console.WriteLine("----- Número máximo de salas cadastradas -----");
+                            Console.ResetColor();
                         }
                         break;
                     case "3":
                         System.Console.WriteLine("Digite o nome do aluno a ser alocado");
                         Console.ReadLine();
-                        
+
                         break;
                     case "4":
                         System.Console.WriteLine("Digite o nome do aluno a ser removido");
                         Console.ReadLine();
-                        
+
                         break;
                     case "5":
                         System.Console.WriteLine("Essas são as salas");
-                        Console.ReadLine();
-                        
+                        for (int a = 0; a < 1; a++)
+                        {
+                            System.Console.WriteLine("----------------------------------------");
+                            System.Console.WriteLine("Número da sala: "+salas[a].numeroSala);
+                            System.Console.WriteLine("Capacidade total: "+salas[a].capacidadeTotal);
+                            System.Console.WriteLine("----------------------------------------");
+                        }
                         break;
                     case "6":
                         System.Console.WriteLine("Esses são os alunos");
-                        System.Console.WriteLine($"");
-                        Console.ReadLine();
-                        
+                        for (int a = 0; a < 4; a++)
+                        {
+                            System.Console.WriteLine(alunos[a].nome);
+                        }
                         break;
                     case "0":
+                        System.Console.WriteLine("Obrigado por acessar!!!");
                         continuar = false;
                         break;
                     default:
-                        System.Console.WriteLine("-----Digite um número válido-----");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.WriteLine("----- Digite um número válido -----");
+                        Console.ResetColor();
                         break;
                 }
             }
--- Senaizinho/Sala.cs
+++ Senaizinho/Sala.cs
@@ -3,8 +3,16 @@ namespace Senaizinho
     public class Sala
     {
         public int numeroSala;
-        public int capacidadeAtual;
-        public int capacidadeTotal;
+        public int capacidadeAtual = 0;
+        public int capacidadeTotal = 1;
         public string[] alunos;
+
+        public void Alocar(string nomeAluno){
+            
+            capacidadeAtual++;
+            capacidadeTotal--;
+            
+        }
     }
+
 }
